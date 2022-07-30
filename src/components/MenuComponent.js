77ef1e4a7efc0a3ac53d7e553661235833ecbc78
Aaron John <root1@Aarons-MacBook-Pro.local>
2022-07-30 13:21:44 +0530

    react components

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { DISHES } from './dishes';
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props); //required whenever you define a class component\
+
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    render() { //implement this method called render() which will return the corresponding view for this component
+
+        const menu = this.state.dishes.map((dish) => {
+            return (
+                <div  className="col-12 col-md-5 m-1">
+                  <Card key={dish.id}
+                    onClick={() => this.onDishSelect(dish)}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                  </Card>
+                </div>
+              );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                        {menu}
+                </div>
+                <div className="row">
+                  <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.state.selectedDish)}
+                  </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
